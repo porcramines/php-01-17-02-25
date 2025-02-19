d39cd996d6ab1882e56accf8ff305aebeff63305
porcramines <porfiriones@gmail.com>
2025-02-19 10:29:08 -0500

    sesion3-1

---
+++ S3/circulo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+<style type="text/css">
+	h1{
+		text-align: center;
+	}
+</style>
+</head>
+<body>
+
+</body>
+</html>
+
+
+<?php
+print "<h1>calculo del area del circulo</h1>";
+print "<h1>---------------------</h1>";
+print "<br>";
+print "<p>el siguiente ejercicio calcula el area del circulo, cuyo radio es -10-: </p>";
+print "<br>";
+$radio=10;
+$area=3.14*10*10;
+print "el area del circulo es:".$area;
+?>
